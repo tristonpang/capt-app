d4394a43f14c33456931357d62c32b8990b8cfb5
tristonpang <t.pang288@hotmail.com>
2019-07-11 19:04:16 +0800

    add list item image, formatting, date/time and venue details

--- src/components/mainFlow/MainListItem.js
+++ src/components/mainFlow/MainListItem.js
@@ -1,20 +1,33 @@
 import React, { Component } from 'react';
-import { Text, TouchableWithoutFeedback } from 'react-native';
+import { View, Text, Image, TouchableWithoutFeedback } from 'react-native';
 import { Actions } from 'react-native-router-flux';
 import { Card, CardSection } from '../common';
 
 class MainListItem extends Component {
     render() {
+        const { title, url, dateTime, venue } = this.props.announcement.item;
+        const { 
+            imageStyle, 
+            titleStyle, 
+            titleContainerStyle, 
+            detailsContainerStyle, 
+            detailsTextStyle 
+        } = styles;
+
         return (
             <Card>
                 <CardSection>
-                    <Text>Event Image Here</Text>
+                    <Image source={{ uri: url }} style={imageStyle} />
                 </CardSection>
 
                 <CardSection>
-                    <Text style={styles.titleStyle}>
-                        {this.props.announcement.item.title}
-                    </Text>
+                    <View style={titleContainerStyle}>
+                        <Text style={titleStyle}>{title}</Text>
+                    </View>
+                    <View style={detailsContainerStyle}>
+                        <Text style={detailsTextStyle}>{dateTime}</Text>
+                        <Text style={detailsTextStyle}>{venue}</Text>
+                    </View>
                 </CardSection>
             </Card>
         );
@@ -22,9 +35,27 @@ class MainListItem extends Component {
 }
 
 const styles = {
+    titleContainerStyle: {
+        justifyContent: 'center'
+    },
     titleStyle: {
         fontSize: 18,
-        paddingLeft: 15
+        fontWeight: 'bold',
+        paddingLeft: 10
+    },
+    imageStyle: {
+        height: 250,
+        flex: 1,
+        width: null
+    },
+    detailsContainerStyle: {
+        flex: 1,
+        justifyContent: 'flex-end',
+        paddingRight: 10,
+        paddingBottom: 5
+    },
+    detailsTextStyle: {
+        textAlign: 'right'
     }
 };
 
