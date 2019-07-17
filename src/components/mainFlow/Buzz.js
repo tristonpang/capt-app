d29a1343633928afdaa795be2e1f4c9ef07bf886
tristonpang <t.pang288@hotmail.com>
2019-07-18 00:00:28 +0800

    add placeholder for Buzz page

--- src/components/mainFlow/Buzz.js
+++ src/components/mainFlow/Buzz.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import { Card, CardSection, Button } from '../common';
+import { Text } from 'react-native';
+import { Card, CardSection } from '../common';
 
 class Buzz extends Component {
     render() {
         return (
             <Card>
                 <CardSection>
-                    <Button>Buzz</Button>
+                    <Text>Under construction! Stay tuned...</Text>
                 </CardSection>
             </Card>
         );
