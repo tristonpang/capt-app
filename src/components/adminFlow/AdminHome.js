68818ba4e4ddee5e4e92092449df93a10cab3810
Jeremy <tan_kai_qun97@yahoo.com.sg>
2019-08-23 01:23:27 +0800

    Fix SafeAreaView ios

--- src/components/adminFlow/AdminHome.js
+++ src/components/adminFlow/AdminHome.js
@@ -1,39 +1,53 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { FlatList } from 'react-native';
-import _ from 'lodash';
-import { adminListFetch } from '../../actions';
-import AdminListItem from './AdminListItem';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { FlatList, View } from "react-native";
+import _ from "lodash";
+import { adminListFetch } from "../../actions";
+import AdminListItem from "./AdminListItem";
+import Button from "../common/Button";
+import CardSection from "../common/CardSection";
 
 class AdminHome extends Component {
-    componentDidMount() {
-        this.props.adminListFetch();
-    }
+  componentDidMount() {
+    this.props.adminListFetch();
+  }
 
-    renderItem(announcement) {
-        return <AdminListItem announcement={announcement} />;
-    }
+  renderItem(announcement) {
+    return <AdminListItem announcement={announcement} />;
+  }
 
-    render() {
-        return (
-            <FlatList 
-                data={this.props.announcements}
-                renderItem={this.renderItem}
-                keyExtractor={announcement => {
-                    if (!announcement.title) {
-                        return 'deleted';
-                    }
-                    return announcement.title.toString();
-                }}
-            />
-        );
-    }
+  onLogOutButtonPress() {}
+
+  render() {
+    return (
+      <View>
+        <FlatList
+          data={this.props.announcements}
+          renderItem={this.renderItem}
+          keyExtractor={announcement => {
+            if (!announcement.title) {
+              return "deleted";
+            }
+            return announcement.title.toString();
+          }}
+        />
+        <CardSection>
+          <Button onButtonPress={() => this.onLogOutButtonPress.bind(this)}>
+            Log Out
+          </Button>
+        </CardSection>
+      </View>
+    );
+  }
 }
 
-const mapStateToProps = (state) => {
-    const { announcements } = state.adminAnnouncements;
+const mapStateToProps = state => {
+  const { announcements } = state.adminAnnouncements;
 
-    return { announcements };
+  return { announcements };
 };
 
-export default connect(mapStateToProps, { adminListFetch })(AdminHome);
+export default connect(
+  mapStateToProps,
+  { adminListFetch }
+)(AdminHome);
--- src/components/mainFlow/MainDetail.js
+++ src/components/mainFlow/MainDetail.js
@@ -1,97 +1,110 @@
-import React, { Component } from 'react';
-import { ScrollView, Image, Text } from 'react-native';
-import { connect } from 'react-redux';
-import { Card, CardSection, Button } from '../common';
-import { signupForEvent, withdrawFromEvent } from '../../actions';
+import React, { Component } from "react";
+import { ScrollView, Image, Text, SafeAreaView } from "react-native";
+import { connect } from "react-redux";
+import { Card, CardSection, Button } from "../common";
+import { signupForEvent, withdrawFromEvent } from "../../actions";
 
 class MainDetail extends Component {
-    onSignupButtonPress() {
-        this.props.signupForEvent(this.props.announcement.key);
+  onSignupButtonPress() {
+    this.props.signupForEvent(this.props.announcement.key);
+  }
+
+  onWithdrawButtonPress() {
+    this.props.withdrawFromEvent(this.props.announcement.key);
+  }
+
+  renderSignupButton() {
+    const { isEvent, isUserSignedUp } = this.props.announcement;
+    const { isFromUserSignups } = this.props;
+
+    if (!isEvent) {
+      return;
     }
 
-    onWithdrawButtonPress() {
-        this.props.withdrawFromEvent(this.props.announcement.key);
+    if (isUserSignedUp || isFromUserSignups) {
+      return (
+        <CardSection>
+          <Button onPress={this.onWithdrawButtonPress.bind(this)}>
+            Withdraw
+          </Button>
+        </CardSection>
+      );
     }
-    
-    renderSignupButton() {
-        const { isEvent, isUserSignedUp } = this.props.announcement;
-        const { isFromUserSignups } = this.props;
 
-        if (!isEvent) {
-            return;
-        }
+    return (
+      <CardSection>
+        <Button onPress={this.onSignupButtonPress.bind(this)}>Sign Up</Button>
+      </CardSection>
+    );
+  }
 
-        if (isUserSignedUp || isFromUserSignups) {
-            return (
-                <CardSection>
-                    <Button onPress={this.onWithdrawButtonPress.bind(this)}>Withdraw</Button>
-                </CardSection>
-            );
-        }
-
-        return (
+  render() {
+    console.log(this.props.announcement);
+    const {
+      url,
+      title,
+      dateTime,
+      venue,
+      description
+    } = this.props.announcement;
+    const {
+      titleDetailsStyle,
+      imageStyle,
+      titleStyle,
+      descriptionStyle
+    } = styles;
+    return (
+      <SafeAreaView>
+        <ScrollView>
+          <Card>
             <CardSection>
-                <Button onPress={this.onSignupButtonPress.bind(this)}>Sign Up</Button>
+              <Image source={{ uri: url }} style={imageStyle} />
             </CardSection>
-        );
-    }
 
-    render() {
-        console.log(this.props.announcement)
-        const { url, title, dateTime, venue, description } = this.props.announcement;
-        const { 
-            titleDetailsStyle,
-            imageStyle,
-            titleStyle,
-            descriptionStyle
-        } = styles;
-        return (
-            <ScrollView>
-                <Card>
-                    <CardSection>
-                        <Image source={{ uri: url }} style={imageStyle} />
-                    </CardSection>
+            <CardSection style={titleDetailsStyle}>
+              <Text style={titleStyle}>{title}</Text>
+              <Text>{dateTime}</Text>
+              <Text>{venue}</Text>
+            </CardSection>
 
-                    <CardSection style={titleDetailsStyle}>
-                        <Text style={titleStyle}>{title}</Text>
-                        <Text>{dateTime}</Text>
-                        <Text>{venue}</Text>
-                    </CardSection>
+            {this.renderSignupButton()}
 
-                    {this.renderSignupButton()}
-
-                    <CardSection>
-                        <Text style={descriptionStyle}>{description}</Text>
-                    </CardSection>
-                </Card>
-            </ScrollView>
-        );
-    }
+            <CardSection>
+              <Text style={descriptionStyle}>{description}</Text>
+            </CardSection>
+          </Card>
+        </ScrollView>
+      </SafeAreaView>
+    );
+  }
 }
 
 const styles = {
-    imageStyle: {
-        height: 500,
-        flex: 1,
-        width: null
-    },
-    titleDetailsStyle: {
-        borderBottomWidth: 1,
-        padding: 5,
-        backgroundCOlor: '#fff',
-        justifyContent: 'flex-start',
-        alignItems: 'center',
-        flexDirection: 'column',
-        borderColor: '#ddd',
-        position: 'relative'
-    },
-    titleStyle: {
-        fontSize: 32,
-        fontWeight: 'bold'
-    },
-    descriptionStyle: {
-        fontSize: 18
-    }
+  imageStyle: {
+    height: 500,
+    flex: 1,
+    width: null
+  },
+  titleDetailsStyle: {
+    borderBottomWidth: 1,
+    padding: 5,
+    backgroundCOlor: "#fff",
+    justifyContent: "flex-start",
+    alignItems: "center",
+    flexDirection: "column",
+    borderColor: "#ddd",
+    position: "relative"
+  },
+  titleStyle: {
+    fontSize: 32,
+    fontWeight: "bold"
+  },
+  descriptionStyle: {
+    fontSize: 18
+  }
 };
 
-export default connect(null, { signupForEvent, withdrawFromEvent })(MainDetail);
+export default connect(
+  null,
+  { signupForEvent, withdrawFromEvent }
+)(MainDetail);
