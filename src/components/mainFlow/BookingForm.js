edab7d0619c85556560b76de4024a90769401b2a
Jeremy <tan_kai_qun97@yahoo.com.sg>
2019-08-24 00:07:02 +0800

    Add ScrowView for booking form

--- src/components/mainFlow/BookingForm.js
+++ src/components/mainFlow/BookingForm.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { ScrollView } from "react-native";
 import DateTimePicker from "react-native-modal-datetime-picker";
 import { email } from "react-native-communications";
 import { Input, Card, CardSection, Button } from "../common";
@@ -126,74 +127,76 @@ class BookingForm extends Component {
 
   render() {
     return (
-      <Card>
-        <CardSection>
-          <Input
-            label="Name"
-            placeholder="John Doe"
-            onChangeText={text =>
-              this.props.bookingFormUpdate({
-                prop: "name",
-                value: text
-              })
-            }
-            value={this.props.name}
-          />
-        </CardSection>
-        <CardSection>
-          <Input
-            label="Phone Number"
-            placeholder="9876 1234"
-            onChangeText={text =>
-              this.props.bookingFormUpdate({
-                prop: "phone",
-                value: text
-              })
-            }
-            value={this.props.phone}
-          />
-        </CardSection>
-        <CardSection>
-          {this.renderDateTimeButton(START_DATETIME)}
-          <DateTimePicker
-            isVisible={this.state.isStartDateTimePickerVisible}
-            onConfirm={this.handleStartDateTimePicked.bind(this)}
-            onCancel={this.hideStartDateTimePicker.bind(this)}
-            mode="datetime"
-          />
-        </CardSection>
-        <CardSection>
-          {this.renderDateTimeButton(END_DATETIME)}
-          <DateTimePicker
-            isVisible={this.state.isEndDateTimePickerVisible}
-            onConfirm={this.handleEndDateTimePicked.bind(this)}
-            onCancel={this.hideEndDateTimePicker.bind(this)}
-            mode="datetime"
-          />
-        </CardSection>
+      <ScrollView>
+        <Card>
+          <CardSection>
+            <Input
+              label="Name"
+              placeholder="John Doe"
+              onChangeText={text =>
+                this.props.bookingFormUpdate({
+                  prop: "name",
+                  value: text
+                })
+              }
+              value={this.props.name}
+            />
+          </CardSection>
+          <CardSection>
+            <Input
+              label="Phone Number"
+              placeholder="9876 1234"
+              onChangeText={text =>
+                this.props.bookingFormUpdate({
+                  prop: "phone",
+                  value: text
+                })
+              }
+              value={this.props.phone}
+            />
+          </CardSection>
+          <CardSection>
+            {this.renderDateTimeButton(START_DATETIME)}
+            <DateTimePicker
+              isVisible={this.state.isStartDateTimePickerVisible}
+              onConfirm={this.handleStartDateTimePicked.bind(this)}
+              onCancel={this.hideStartDateTimePicker.bind(this)}
+              mode="datetime"
+            />
+          </CardSection>
+          <CardSection>
+            {this.renderDateTimeButton(END_DATETIME)}
+            <DateTimePicker
+              isVisible={this.state.isEndDateTimePickerVisible}
+              onConfirm={this.handleEndDateTimePicked.bind(this)}
+              onCancel={this.hideEndDateTimePicker.bind(this)}
+              mode="datetime"
+            />
+          </CardSection>
 
-        {this.renderSpecificFields()}
+          {this.renderSpecificFields()}
 
-        <CardSection>
-          <Input
-            label="Description of Activities"
-            placeholder="description"
-            onChangeText={text =>
-              this.props.bookingFormUpdate({
-                prop: "description",
-                value: text
-              })
-            }
-            value={this.props.description}
-          />
-        </CardSection>
+          <CardSection>
+            <Input
+              label="Description of Activities"
+              placeholder="description"
+              onChangeText={text =>
+                this.props.bookingFormUpdate({
+                  prop: "description",
+                  value: text
+                })
+              }
+              value={this.props.description}
+            />
+          </CardSection>
 
-        <CardSection>
-          <Button onPress={this.onSendRequestPress.bind(this)}>
-            Send Booking Request
-          </Button>
-        </CardSection>
-      </Card>
+          <CardSection>
+            <Button onPress={this.onSendRequestPress.bind(this)}>
+              Send Booking Request
+            </Button>
+          </CardSection>
+        </Card>
+      </ScrollView>
     );
   }
 }
